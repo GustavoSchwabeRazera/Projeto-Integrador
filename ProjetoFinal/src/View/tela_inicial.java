43fec989ad901f77ebfe6c07ccf121b39711083b
unknown <guilhermeedelizz@gmail.com>
2026-08-11 08:43:33 -0300

    merge 11/08

--- ProjetoFinal/src/View/tela_inicial.java
+++ ProjetoFinal/src/View/tela_inicial.java
@@ -14,6 +14,7 @@ import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.Component;
 	
 	public class tela_inicial extends JFrame {
 	
@@ -41,22 +42,23 @@ import java.awt.Font;
 		 */
 		public tela_inicial() {
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setBounds(100, 100, 1330, 721);
+			setBounds(100, 100, 1920, 1080);
 			contentPane = 	new JPanel();
+			contentPane.setAlignmentX(Component.LEFT_ALIGNMENT);
 			contentPane.setBackground(new Color(175, 244, 198));
 			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 			setContentPane(contentPane);
-			contentPane.setLayout(new MigLayout("", "[233.00,grow][200,grow][200,grow][302.00,grow][138.00,grow]", "[113.00,grow][][grow][grow][grow][grow][grow][71.00,grow][54.00,grow][grow]"));
+			contentPane.setLayout(new MigLayout("", "[480,grow][480][480,grow][480,grow][261.00,grow][153.00]", "[113.00,grow][][][grow][grow][grow][grow][grow][71.00,grow][54.00,grow][grow]"));
 			
 			//logo
 			JLabel lblNewLabel_3 = new JLabel("");
 
 			ImageIcon logo = new ImageIcon(tela_inicial.class.getResource("/imagens/Logo.png"));
-			Image imagemRedimensionada = logo.getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+			Image imagemRedimensionada = logo.getImage().getScaledInstance(295, 150, Image.SCALE_SMOOTH);
 
 			lblNewLabel_3.setIcon(new ImageIcon(imagemRedimensionada));
 
-			contentPane.add(lblNewLabel_3, "flowy,cell 0 0,alignx left,aligny top");
+			contentPane.add(lblNewLabel_3, "flowy,cell 0 0,alignx left");
 			
 			//pesquisar
 			JButton btnPesquisar = new JButton();
@@ -67,51 +69,10 @@ import java.awt.Font;
 			
 			//pesquisar
 			ImageIcon pesquisar = new ImageIcon(tela_inicial.class.getResource("/imagens/pesquisar.png"));
-			Image imgPesquisar = pesquisar.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+			Image imgPesquisar = pesquisar.getImage().getScaledInstance(295, 115, Image.SCALE_SMOOTH);
 			
 			JLabel lblNewLabel_3_2 = new JLabel("");
 			contentPane.add(lblNewLabel_3_2, "flowx,cell 4 0");
-			
-			JLabel lblNewLabel = new JLabel("Bem-Vindo!");
-			lblNewLabel.setForeground(new Color(10, 86, 27));
-			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 51));
-			contentPane.add(lblNewLabel, "cell 0 2 5 1,alignx center");
-
-			btnPesquisar.setIcon(new ImageIcon(imgPesquisar));
-			btnPesquisar.setBorderPainted(false);
-			btnPesquisar.setContentAreaFilled(false);
-			btnPesquisar.setFocusPainted(false);
-
-			contentPane.add(btnPesquisar, "cell 1 5,alignx center");
-			
-			//meus livros
-			JButton btnMeusLivros = new JButton();
-
-			ImageIcon livros = new ImageIcon(tela_inicial.class.getResource("/imagens/meus livros.png"));
-			Image imgLivros = livros.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
-
-			btnMeusLivros.setIcon(new ImageIcon(imgLivros));
-			btnMeusLivros.setBorderPainted(false);
-			btnMeusLivros.setContentAreaFilled(false);
-			btnMeusLivros.setFocusPainted(false);
-
-			contentPane.add(btnMeusLivros, "cell 2 5,alignx center");			
-			
-			//solicitacoes
-			JButton btnSolicitacoes = new JButton();
-
-			ImageIcon solicitacoes = new ImageIcon(tela_inicial.class.getResource("/imagens/solicitacoes.png"));
-			Image imgSolicitacoes = solicitacoes.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
-
-			btnSolicitacoes.setIcon(new ImageIcon(imgSolicitacoes));
-			btnSolicitacoes.setBorderPainted(false);
-			btnSolicitacoes.setContentAreaFilled(false);
-			btnSolicitacoes.setFocusPainted(false);
-
-			contentPane.add(btnSolicitacoes, "cell 3 5,alignx center");			
-			
-			JLabel lblNewLabel_3_1 = new JLabel("");
-			contentPane.add(lblNewLabel_3_1, "cell 0 0");
 			
 			//perfil
 			JButton btnPerfil = new JButton();
@@ -120,16 +81,68 @@ import java.awt.Font;
 				public void actionPerformed(ActionEvent e) {
 				}
 			});
+			
+					
+						
+						JButton btnNewButton = new JButton("");
+						btnNewButton.setBorderPainted(false);
+						btnNewButton.setIcon(new ImageIcon(tela_inicial.class.getResource("/imagens/FotoPerfil.png")));
+						btnNewButton.setContentAreaFilled(false);
+						contentPane.add(btnNewButton, "flowy,cell 5 0");
+						btnPerfil.setIcon(new ImageIcon());
+						btnPerfil.setBorderPainted(false);
+						btnPerfil.setContentAreaFilled(false);
+						btnPerfil.setFocusPainted(false);
+						
+									contentPane.add(btnPerfil, "cell 5 0,growy");
+			
+			JLabel lblNewLabel = new JLabel("Bem-Vindo!");
+			lblNewLabel.setForeground(new Color(10, 86, 27));
+			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 51));
+			contentPane.add(lblNewLabel, "cell 2 3,alignx center");
+
+			btnPesquisar.setIcon(new ImageIcon(imgPesquisar));
+			btnPesquisar.setBorderPainted(false);
+			btnPesquisar.setContentAreaFilled(false);
+			btnPesquisar.setFocusPainted(false);
+
+			contentPane.add(btnPesquisar, "cell 1 6,alignx center");
+			
+			//meus livros
+			JButton btnMeusLivros = new JButton();
+			btnMeusLivros.setAlignmentX(Component.RIGHT_ALIGNMENT);
+			btnMeusLivros.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+
+			ImageIcon livros = new ImageIcon(tela_inicial.class.getResource("/imagens/meus livros.png"));
+			Image imgLivros = livros.getImage().getScaledInstance(295, 115, Image.SCALE_SMOOTH);
+
+			btnMeusLivros.setIcon(new ImageIcon(imgLivros));
+			btnMeusLivros.setBorderPainted(false);
+			btnMeusLivros.setContentAreaFilled(false);
+			btnMeusLivros.setFocusPainted(false);
+
+			contentPane.add(btnMeusLivros, "cell 2 6,alignx center");			
+			
+			//solicitacoes
+			JButton btnSolicitacoes = new JButton();
+
+			ImageIcon solicitacoes = new ImageIcon(tela_inicial.class.getResource("/imagens/solicitacoes.png"));
+			Image imgSolicitacoes = solicitacoes.getImage().getScaledInstance(295, 115, Image.SCALE_SMOOTH);
+
+			btnSolicitacoes.setIcon(new ImageIcon(imgSolicitacoes));
+			btnSolicitacoes.setBorderPainted(false);
+			btnSolicitacoes.setContentAreaFilled(false);
+			btnSolicitacoes.setFocusPainted(false);
+
+			contentPane.add(btnSolicitacoes, "cell 3 6,alignx center");			
+			
+			JLabel lblNewLabel_3_1 = new JLabel("");
+			contentPane.add(lblNewLabel_3_1, "cell 0 0");
 
 			ImageIcon perfil = new ImageIcon(tela_inicial.class.getResource("/imagens/perfil3.png"));
-			Image imgPerfil = perfil.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-
-			btnPerfil.setIcon(new ImageIcon(imgPerfil));
-			btnPerfil.setBorderPainted(false);
-			btnPerfil.setContentAreaFilled(false);
-			btnPerfil.setFocusPainted(false);
-
-			contentPane.add(btnPerfil, "cell 4 0,alignx right,aligny top");
 			
 			JLabel lblNewLabel_1 = new JLabel("");
 			contentPane.add(lblNewLabel_1, "cell 4 0");
@@ -152,6 +165,15 @@ import java.awt.Font;
 			Image imgHistorico = historico.getImage().getScaledInstance(
 			    170, 50, Image.SCALE_SMOOTH
 			);
+
+			// tamanho da imagem
+			ImageIcon calendario = new ImageIcon(
+				tela_inicial.class.getResource("/imagens/calendario.png")
+			);
+
+			Image imgCalendario = calendario.getImage().getScaledInstance(
+				75, 75, Image.SCALE_SMOOTH
+			);
 			
 			// calendário
 			JButton btnCalendario = new JButton("");
@@ -162,35 +184,15 @@ import java.awt.Font;
 			btnCalendario.setContentAreaFilled(false);
 			btnCalendario.setBorderPainted(false);
 			btnCalendario.setFocusPainted(false);
-
-			// tamanho da imagem
-			ImageIcon calendario = new ImageIcon(
-				tela_inicial.class.getResource("/imagens/calendario.png")
-			);
-
-			Image imgCalendario = calendario.getImage().getScaledInstance(
-				50, 50, Image.SCALE_SMOOTH
-			);
-
-			btnCalendario.setIcon(new ImageIcon(imgCalendario));
-
-			contentPane.add(btnCalendario, "cell 4 7,alignx right");
+			
+						btnCalendario.setIcon(new ImageIcon(imgCalendario));
+						
+									contentPane.add(btnCalendario, "cell 5 8,alignx center");
 			
 			//historico
 			btnHistorico.setIcon(new ImageIcon(imgHistorico));
 
-			contentPane.add(btnHistorico, "cell 0 8,alignx left,aligny bottom");
-			
-			// notificação
-			JButton btnNotificacao = new JButton("");
-			btnNotificacao.setContentAreaFilled(false);
-			btnNotificacao.setBorderPainted(false);
-			btnNotificacao.setFocusPainted(false);
-
-			btnNotificacao.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
+			contentPane.add(btnHistorico, "cell 0 9,alignx left,aligny top");
 
 			// tamanho da imagem
 			ImageIcon notificacao = new ImageIcon(
@@ -198,11 +200,22 @@ import java.awt.Font;
 			);
 
 			Image imgNotificacao = notificacao.getImage().getScaledInstance(
-				50, 50, Image.SCALE_SMOOTH
+				75, 75, Image.SCALE_SMOOTH
 			);
-
-			btnNotificacao.setIcon(new ImageIcon(imgNotificacao));
-
-			contentPane.add(btnNotificacao, "cell 4 8,alignx right");
+			
+			// notificação
+			JButton btnNotificacao = new JButton("");
+			btnNotificacao.setContentAreaFilled(false);
+			btnNotificacao.setBorderPainted(false);
+			btnNotificacao.setFocusPainted(false);
+			
+						btnNotificacao.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+							}
+						});
+						
+									btnNotificacao.setIcon(new ImageIcon(imgNotificacao));
+									
+												contentPane.add(btnNotificacao, "cell 5 9,alignx center");
 	
 	}}
