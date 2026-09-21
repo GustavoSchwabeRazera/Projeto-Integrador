a05c14c264781c68db010191a80e48c6295556c1
guilherme <guilhermeedelizz@gmail.com>
2026-09-21 10:06:31 -0300

    dados aleatrios no banco de dados

--- ProjetoFinal/database.sql
+++ ProjetoFinal/database.sql
@@ -56,3 +56,48 @@ data_termino DATE NOT NULL,
 FOREIGN KEY (CPF_usuario) REFERENCES Usuarios(CPF),
 FOREIGN KEY (ISBN) REFERENCES Livros(ISBN)
 );
+
+INSERT INTO Usuarios 
+(CPF, nome, telefone, email, data_nascimento, senha)
+VALUES
+('12345678911', 'guilherme', '999999', 'gui@gmail', '2008-09-12', '123456'),
+('98765432100', 'Mariana', '988888888', 'mariana@gmail.com', '2007-05-23', 'senha123'),
+('45678912300', 'Lucas', '977777777', 'lucas@gmail.com', '2009-11-08', 'lucas123');
+
+
+
+--dados aleatórios para teste ->
+-- LIVROS
+INSERT INTO Livros
+(ISBN, fotoContraCapa, fotoCapa, status, CPF_dono, titulo, data_lancamento)
+VALUES
+('9788535902777', NULL, NULL, TRUE, '12345678911', 'O Hobbit', '1937-09-21'),
+('9788532530788', NULL, NULL, TRUE, '98765432100', 'Dom Casmurro', '1899-01-01');
+
+
+-- AUTORES
+INSERT INTO Autor
+(id_autor, nome, nacionalidade)
+VALUES
+(1, 'J. R. R. Tolkien', 'Britânica'),
+(2, 'Machado de Assis', 'Brasileira'),
+(3, 'Clarice Lispector', 'Brasileira'),
+(4, 'George Orwell', 'Britânica');
+
+
+-- LIVROS E SEUS AUTORES
+INSERT INTO Pertence
+(ISBN, id_autor)
+VALUES
+('9788535902777', 1),
+('9788532530788', 2);
+
+
+-- EMPRÉSTIMOS
+INSERT INTO Emprestimos
+(id_emprestimo, CPF_usuario, ISBN, statusEmprestimo, data_inicio, data_termino)
+VALUES
+(1, '45678912300', '9788535902777', TRUE, '2026-09-15', '2026-09-29'),
+(2, '12345678911', '9788532530788', FALSE, '2026-09-01', '2026-09-15');
+
+
